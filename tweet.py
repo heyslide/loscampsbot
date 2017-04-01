f78748e7f055178359fcf8f3af3cf7faed3f0e5e
heyslide <gorka.pinol@gmail.com>
2017-04-02 01:50:46 +0200

    Finished lyrics, small change

--- tweet.py
+++ tweet.py
@@ -1,12 +1,12 @@
-import time, tweepy
+import time, tweepy, config
 from linereader import copen
 from random import randint
 
 """tweepy doing its magic"""
-CONSUMER_KEY = 'bM86WKsdFEJgrWlBrUY3KCSni'
-CONSUMER_SECRET = '8T43isXZlIIwlOxrwPLg0AAajWjLlNvfTDydkzAVIFRHfvq7Fg'
-ACCESS_KEY = '848238932328402945-Kvzj1xXS3eurZOPOVaXP9drkIB4jOw4'
-ACCESS_SECRET = 'hdEPrSMGGKK2PXidlhFHWvaWNHiUeSjeUu56PMsj2pxNv'
+CONSUMER_KEY = config.ckey
+CONSUMER_SECRET = config.csecret
+ACCESS_KEY = config.akey
+ACCESS_SECRET = config.asecret
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -20,4 +20,4 @@ while True:
     if len(tweet_text) <= 140 and tweet_text != '\n':
 	api.update_status(status=tweet_text)
 	print tweet_text
-	time.sleep(20*60)
+	time.sleep(1200)
